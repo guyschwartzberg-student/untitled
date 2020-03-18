8bb0f1f5e28da35a056bc61471fca92806226a28
GuySchwartzberg <bojanvrucina@gmail.com>
2020-03-18 11:04:50 +0200

    qwe

--- what.js
+++ what.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Hi() {
-    return <div>Hello Worlasdd!</div>;
+    return <div>Hello Worlasddfsdd!</div>;
 }
 
 ReactDOM.render(<Hi/>, document.querySelector('#root'));
